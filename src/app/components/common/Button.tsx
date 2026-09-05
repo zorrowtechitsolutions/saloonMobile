924d9bd90613a409567d9ef6918a1e3b891febc3
FARHAN <farhan@FARHANs-MacBook-Air.local>
2026-09-05 22:49:15 +0530

    feat (profile)

--- src/app/components/common/Button.tsx
+++ src/app/components/common/Button.tsx
@@ -4,14 +4,16 @@ export default function ButtonComponent({
   buttonText,
   textColor,
   bgColor,
+  boder,
 }: {
   buttonText?: string;
   textColor?: string;
   bgColor?: string;
+  boder?: string;
 }) {
   return (
     <Pressable
-      className={`h-12 flex-1 items-center justify-center rounded-[10px] ${bgColor}`}
+      className={`h-12 flex-1 items-center justify-center border ${boder}  rounded-[10px] ${bgColor}`}
     >
       <Text className={`text-[17px] font-semibold ${textColor}`}>
         {buttonText}
